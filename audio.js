9b785d80e8b7f42174f315d52a55120550a1f62f
Emily Gordon <emgordon154@gmail.com>
2017-11-20 16:21:07 -0500

    got quacking working!!!

--- audio.js
+++ audio.js
@@ -1,3 +1,15 @@
+// var Player = require('player')
+
+// var player = new Player('./quacks.mp3')
+
+// player.on('error', (err) => {
+//   console.log(err)
+// })
+
+// module.exports = player
+
+
+
 var IPod = require('ipod')
 
 var ipod = new IPod('./quacks.mp3')
@@ -9,4 +21,14 @@ ipod.on('playing', (item) => {
 // make the duck quack with 
 //   ipod.play()
 
+ipod.on('error', (err) => {
+  console.log(err)
+})
+
+ipod.on('playend', (item) => {
+  ipod.stop()
+})
+
 module.exports = ipod
+
+
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const Express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
-const play = require('audio-play')
-const load = require('audio-loader')
+var player = require('./audio.js')
 
-const app = Express()
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.get('/', (req,res,next) => {
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+console.log(player.list)
+
+app.get('/', (req, res, next) => {
+  // create quack sound on
+  // res.send("I'm a working, non-quacking server");
+  // res.sendFile(path.join(__dirname, 'index.html'));
   
-})
-
+  player.play()
+  setTimeout(()=>{player.stop()}, 2000)
+  res.status(200).send()
+});
