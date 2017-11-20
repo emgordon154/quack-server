443517ecb4bfdce3d8c1f4af209541fa3086b546
Emily Gordon <emgordon154@gmail.com>
2017-11-20 15:18:15 -0500

    began writing a basic Express server, handing off the file

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Express = require('express')
 const morgan = require('morgan')
 
+const play = require('audio-play')
+const load = require('audio-loader')
+
 const app = Express()
 
 app.use(morgan('dev'))
+
+app.get('/', (req,res,next) => {
+  
+})
 
